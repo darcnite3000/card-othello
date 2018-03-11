bd74e942287047ebe5c4816f45b44605ccad89ce
Tristan Smith <darcnite3000@gmail.com>
2018-03-12 10:40:00 +1100

    allow for suitfaces to not have a value

--- src/Diamond.jsx
+++ src/Diamond.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
-const Diamond = ({value = 0,width = 30, height=30, stroke="red", text="white", strokeWidth="2", ...rest}) => (
+const Diamond = ({
+  value,
+  width = 30,
+  height = 30,
+  stroke = 'red',
+  text = 'white',
+  strokeWidth = '2',
+  ...rest
+}) => (
   <svg
     width={width}
     height={height}
@@ -11,7 +19,18 @@ const Diamond = ({value = 0,width = 30, height=30, stroke="red", text="white", s
     {...rest}
   >
     <path d="m3.05481,20.76236c3.7795,-7.16059 10.59373,-13.27216 16.39076,-19.33748c6.74361,3.89234 12.56971,12.45541 17.63846,18.4194c-4.52098,6.89842 -11.02885,13.18099 -16.89158,19.36765c-6.32013,-5.66022 -11.72487,-12.12712 -17.13764,-18.44958l0,0z" />
-    <text stroke="none" style={{fontSize:19}} fill={text} textAnchor="middle" x="20" y="28">{value}</text>
+    {value != undefined ? (
+      <text
+        stroke="none"
+        style={{ fontSize: 19 }}
+        fill={text}
+        textAnchor="middle"
+        x="20"
+        y="28"
+      >
+        {value}
+      </text>
+    ) : null}
   </svg>
 )
 
--- src/Heart.jsx
+++ src/Heart.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
-const Heart = ({value = 0,width = 30, height=30, stroke="red", text="white", strokeWidth="2", ...rest}) => (
+const Heart = ({
+  value,
+  width = 30,
+  height = 30,
+  stroke = 'red',
+  text = 'white',
+  strokeWidth = '2',
+  ...rest
+}) => (
   <svg
     width={width}
     height={height}
@@ -11,7 +19,18 @@ const Heart = ({value = 0,width = 30, height=30, stroke="red", text="white", str
     {...rest}
   >
     <path d="m14.51917,34.13318c-9.81414,-8.69721 -13.32308,-14.1989 -13.35152,-20.93398c-0.02593,-6.13984 4.30568,-12.0657 8.79463,-12.03152c2.24158,0.0171 7.04992,2.2769 8.75257,4.11355c0.85789,0.92541 1.26424,0.83416 3.17425,-0.71286c5.19824,-4.21033 10.27438,-4.2987 13.56191,-0.23609c5.25417,6.49291 4.29759,14.24662 -2.74984,22.28932c-3.74682,4.27597 -11.92906,11.86177 -12.79439,11.86177c-0.26331,0 -2.68773,-1.95759 -5.3876,-4.35018l0,0z" />
-    <text stroke="none" style={{fontSize:19}} fill={text} textAnchor="middle" x="20" y="28">{value}</text>
+    {value != undefined ? (
+      <text
+        stroke="none"
+        style={{ fontSize: 19 }}
+        fill={text}
+        textAnchor="middle"
+        x="20"
+        y="28"
+      >
+        {value}
+      </text>
+    ) : null}
   </svg>
 )
 
