603a3d686e5b5490979ab9e8b78d7d6bb3e76963
Tristan Smith <darcnite3000@gmail.com>
2018-03-12 11:31:01 +1100

    add layout to cell

--- src/BoardCell.jsx
+++ src/BoardCell.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Card } from './Card'
+import { suitFace } from './suits'
 
-const BoardCell = props => (
-  <code style={{ margin: '5px', padding: '5px', backgroundColor: 'wheat' }}>
-    <pre>{JSON.stringify(props, null, 2)}</pre>
-  </code>
-)
+function getStatus(cellSuit, cardSuit) {
+  return cellSuit === -1 ? 0 : cellSuit === cardSuit ? 1 : -1
+}
+const LockedCell = () => <div className="cell-base-locked" />
+const EmptyCell = () => <div className="cell-base-empty" />
+
+const BoardCell = ({ suit = -1, card, style, locked = false }) => {
+  const BaseCell = locked
+    ? LockedCell
+    : suit === -1 ? EmptyCell : suitFace[suit]
+  return (
+    <div
+      className={`cell${locked ? ' cell-locked' : ''}${
+        suit !== -1 ? ` cell-suit-${suit}` : ''
+      }`}
+      style={style}
+    >
+      <div className="cell-base">
+        <BaseCell />
+      </div>
+      <div className="cell-card">
+        {card && <Card {...card} status={getStatus(suit, card.suit)} />}
+      </div>
+    </div>
+  )
+}
 
 export default BoardCell
