6d76251dcd26b3ecb27bbf77b1fdd0467e447d00
Tristan Smith <darcnite3000@gmail.com>
2018-06-08 14:11:58 +1000

    move old POC

---
+++ src/old-POC/suits.js
@@ -0,0 +1,19 @@
+import { range } from '../lib/utils'
+import Heart from '../components/Heart'
+import Club from '../components/Club'
+import Diamond from '../components/Diamond'
+import Spade from '../components/Spade'
+
+export const suitFace = [Diamond, Club, Heart, Spade]
+export const cardInitial = {
+  1: 'A',
+  10: 'T',
+  11: 'J',
+  12: 'Q',
+  13: 'K'
+}
+
+export const minValue = 1
+export const maxValue = 13
+export const values = range(minValue, maxValue)
+export const suits = range(0, suitFace.length - 1)
